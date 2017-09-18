6af8ad0843ce0cac7594b4aef950abb58783db0b
mpascualach <miguelpacheson@hotmail.es>
2017-09-18 14:05:08 +0200

    auth done

--- app.js
+++ app.js
@@ -1,18 +1,54 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const expressLayouts = require('express-ejs-layouts');
+const session = require("express-session");
+const passport = require("passport");
+const flash = require("connect-flash");
+const MongoStore = require("connect-mongo")(session);
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const authRoutes = require('./routes/auth');
+
+const debug = require('debug')("app:"+path.basename(__filename).split('.')[0]);
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/passport-local",{useMongoClient:true})
+        .then(()=> debug("connected to db!"));
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('layout','layout');
+app.use(expressLayouts);
+
+app.use(flash());
+
+app.use((req,res,next) =>{
+  res.locals.title = "Auth example";
+  next();
+});
+
+app.use(session({
+  secret: "our-passport-local-strategy-app",
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60 // 1 day
+  })
+}));
+
+require('./passport/serializers');
+require('./passport/local');
+require('./passport/facebook');
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +58,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+app.use('/', authRoutes);
+app.get('/', (req,res) => res.render('index',{user:req.user}));
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
