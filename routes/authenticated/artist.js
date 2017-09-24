953f7fd8d022fe6d20fa345659354435c82049c1
Luisca108 <luiscarlos3106@gmail.com>
2017-09-24 12:22:41 +0200

    Search working

--- routes/authenticated/artist.js
+++ routes/authenticated/artist.js
@@ -30,34 +30,33 @@ router.get("/artist/:id", (req, res, next) => {
     });
 });
 
-router.get("/artists", (req, res, next) => {
-  var content = [];
-  console.log('apiArtist-list');
-  for (var i = 0; i < 10; i++){
-    api
-      .newRequest()
-      .follow("artist")
-      .withRequestOptions({
-        headers: {
-          "X-Xapp-Token": xappToken,
-          Accept: "application/vnd.artsy-v2+json"
-        }
-      })
-      .withTemplateParameters({
-        name: req.params.name
-      })
-      .getResource(function(error, artist) {
-        console.log(artist)
-        content.push(artist.name);
-      });
-  }
-  console.log(content)
-  res.render("artists", {artists: content})
-});
+// router.get("/artists", (req, res, next) => {
+
+//     api
+//       .newRequest()
+//       .follow("artist")
+//       .withRequestOptions({
+//         headers: {
+//           "X-Xapp-Token": xappToken,
+//           Accept: "application/vnd.artsy-v2+json"
+//         }
+//       })
+//       .withTemplateParameters({
+//         name: req.query.keyword
+//       })
+//       .getResource(function(error, artist) {
+//         console.log(artist._embedded.artists);
+//           res.render("artists", {
+//                                   artists: artist._embedded.artists
+//                                 });
+
+//       });
+  
+// });
 
 
 
-router.get("/example",(req,res,next)=>{
+router.get("/search",(req,res,next)=>{
   api2 = traverson.from(`https://api.artsy.net/api/search?q=${req.query.search}`).jsonHal();
   console.log(req.query.search);
   api2
@@ -71,8 +70,12 @@ router.get("/example",(req,res,next)=>{
     .withTemplateParameters({
       name: ""
     })
-    .getResource(function(error, artist){
-      console.log(artist._embedded.results[4]);
+    .getResource(function(error, result){
+      console.log(Object.values(result._embedded.results[0]._links.permalink)[0]);
+      console.log(result._embedded.results[0].title_links);
+      res.render("search", { 
+        results: result._embedded.results
+      });
     });
 });
 
